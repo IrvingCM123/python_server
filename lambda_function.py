4557e0962f3c33a049059d16eb43defe2011d6aa
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-08-21 18:10:15 -0600

    definir funcion para buscar personajes

--- lambda_function.py
+++ lambda_function.py
@@ -6,3 +6,22 @@ import os
 # Configurar logger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+
+# Configurar el URL 
+API_URL = os.getenv('API_URL')
+
+# Buscar personajes 
+def buscar_personajes():
+    # Agregar sufijo a la url almacenada
+    url = API_URL + '/character'
+    
+    # Realizar consulta
+    try:
+        response = request.get(url)
+        response.raise_for_status()
+        #Retornar respuesta
+        return response.json()
+    except request.exceptions.RequestException as e:
+        logger.error(f' Ha ocurrido un error al realizar la consula: {e}')
+        raise 
